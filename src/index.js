c40a9908d3e569e3d785bfc7bd5afdc8e894f3fa
unknown <raffi.ssj@gmail.com>
2020-07-03 21:35:34 +0200

    create Firebase commponents

--- src/index.js
+++ src/index.js
@@ -4,24 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import firebase from 'firebase'
-import 'firebase/auth'
-import 'firebase/app'
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  apiId: process.env.REACT_APP_API_ID
-}
-
-firebase.initializeApp(firebaseConfig);
-
-firebase.auth();
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
